34b6688013e44e0a78b502ac7c9259625ccfe633
SasiaDagnoli <sasi0026@stud.kea.dk>
2022-05-07 20:51:22 +0200

    Swiper skeleton made

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,3 +1,9 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
+
 export default function Frontpage() {
   return (
     <div>
@@ -10,6 +16,44 @@ export default function Frontpage() {
       </section>
       <section>
         <h1>Populære produkter</h1>
+        <Swiper
+          slidesPerView={3.8}
+          spaceBetween={30}
+          pagination={{
+            el: ".custom-pagination",
+            clickable: true,
+          }}
+          grabCursor={true}
+          loop={true}
+          centeredSlides={true}
+          loopFillGroupWithBlank={true}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="swiper"
+        >
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+        </Swiper>
+        <div className="custom-pagination"></div>
+      </section>
+      <section>
+        <h1>Aflever dine smykker</h1>
       </section>
     </div>
   );
