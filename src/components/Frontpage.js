138ba80341b5e39aebd1cd9d6b65e94aaa595975
SasiaDagnoli <sasi0026@stud.kea.dk>
2022-05-07 23:54:36 +0200

    Frontpage skeleton done

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -7,12 +7,13 @@ import { Pagination, Navigation } from "swiper";
 export default function Frontpage() {
   return (
     <div>
-      <section>
-        <img
+      <section id="first-section">
+        {/* <img
           src={`./images/placeholder_background.jpg`}
           alt=""
           className="background-image"
-        />
+        /> */}
+        <button>Shop Nu</button>
       </section>
       <section>
         <h1>Populære produkter</h1>
@@ -69,6 +70,53 @@ export default function Frontpage() {
           src={`./images/kunderejse.png`}
           alt=""
         />
+      </section>
+      <section>
+        <h1>Gå på opdagelse</h1>
+        <Swiper
+          breakpoints={{
+            640: {
+              width: 640,
+              slidesPerView: 1,
+            },
+            768: {
+              width: 768,
+              slidesPerView: 2.1,
+            },
+          }}
+          slidesPerView={1}
+          spaceBetween={30}
+          pagination={{
+            el: ".custom-pagination",
+            clickable: true,
+          }}
+          grabCursor={true}
+          loop={true}
+          centeredSlides={true}
+          loopFillGroupWithBlank={true}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="swiper"
+        >
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={`./images/placeholder_products.jpg`} alt="" />
+          </SwiperSlide>
+        </Swiper>
       </section>
     </div>
   );
