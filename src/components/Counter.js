3a6dfcf73ac5325c3fdf908177f7c03ac7d40596
linegommesen <line090g@stud.kea.dk>
2022-05-16 14:49:24 +0200

    counter with one item functionality

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,12 @@
 export default function Counter(props) {
-  //   const item = props.basket.find((basketItem) => basketItem.productname === props.product.productname);
+  // const item = props.basket.find((basketItem) => basketItem.productname === props.product.productname);
 
-  //   let amount = 0;
-  //   if (item && item.amount) {
-  //     amount = item.amount;
-  //   }
+  const item = props.product.productname;
+
+  let amount = 0;
+  if (item && item.amount) {
+    amount = item.amount;
+  }
 
   function handleClick() {
     props.setBasket((old) => {
@@ -12,10 +14,16 @@ export default function Counter(props) {
       const nextState = [...old].map((item) => {
         if (item.productname === props.product.productname) {
           found = true;
-          return { ...item, amount: item.amount + 1 };
+          return { ...item, amount: item.amount };
         }
+        console.log("put i kurv", item);
         return item;
       });
+      if (found) {
+        return nextState;
+      } else {
+        return nextState.concat({ productname: props.product.productname, price: props.product.price, amount: 1 });
+      }
     });
   }
   return (
