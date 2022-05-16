1700e75bf5e775375eed4099cb84ee31b5a7c699
linegommesen <line090g@stud.kea.dk>
2022-05-16 15:02:02 +0200

    basket full functionality

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,12 +2,14 @@ import React from "react";
 import { slide as Basket } from "react-burger-menu";
 import "../styles/sidebar.css";
 import ProductBasket from "./ProductBasket";
+import MyBasket from "./MyBasket";
 
 export default (props) => {
   return (
     <Basket className="basket" right>
       <h2>Your basket:</h2>
       <ProductBasket basket={props.basket}></ProductBasket>
+      <MyBasket basket={props.basket} />
     </Basket>
   );
 };
--- src/components/ProductBasket.js
+++ src/components/ProductBasket.js
@@ -1,3 +1,14 @@
 export default function ProductBasket(props) {
-  return <section>Basket</section>;
+  const basketContent = props.basket.map((product, index) => {
+    return (
+      <div>
+        <article key={index}>
+          <h3>{product.productname}</h3>
+          <p>Pris: {product.price} DKK</p>
+          <p>Antal: {product.amount}</p>
+        </article>
+      </div>
+    );
+  });
+  return <section>{basketContent}</section>;
 }
