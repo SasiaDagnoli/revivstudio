2af8664697474dce795c1f0cf31209b01b70433e
SasiaDagnoli <sasi0026@stud.kea.dk>
2022-05-21 00:36:50 +0200

    small styling changes

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,7 +3,7 @@ import Productlist from "./Productlist";
 export default function Shop(props) {
   return (
     <div className="Shop">
-      <h1>shop</h1>
+      <h1>Alle smykker</h1>
       <Productlist
         products={props.products}
         basket={props.basket}
