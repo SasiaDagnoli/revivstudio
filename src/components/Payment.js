d47c2cf71e073ff8d198ebb4e465ad1dabe351f7
linegommesen <line090g@stud.kea.dk>
2022-05-25 13:58:37 +0200

    payment form styling

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -21,29 +21,41 @@ export default function Payment() {
         <form action="">
           <div className="form-control">
             <input type="radio" name="paymentmethod" id="mobilepay" />
-            <label htmlFor="mobilepay">Mobilepay</label>
+            <label htmlFor="mobilepay">
+              <img className="mp-logo" src={`./images/mobilepay_logo_small.png`} alt="" />
+            </label>
+          </div>
+          <div className="mobilepay-form">
+            <label htmlFor="phonenumber">Telefon nr.*</label>
+            <input type="tel" id="phonenumber" />
           </div>
           <div className="form-control">
             <input type="radio" name="paymentmethod" id="kort" />
             <label htmlFor="kort">Kortbetaling</label>
           </div>
           <div className="card-form">
-            <label htmlFor="name">Kortholders navn</label>
-            <input type="text" id="name" />
-            <label htmlFor="cardnumber">Kortnummer</label>
-            <input type="tel" id="cardnumber" />
-            <label htmlFor="expiry">Gyldig til</label>
-            <input type="text" id="expiry" />
-            <label htmlFor="cc">Kontrolcifre</label>
-            <input type="text" id="cc" />
+            <label htmlFor="name">Kortholders navn*</label>
+            <input required minLength="1" autoFocus type="text" id="name" />
+            <label htmlFor="cardnumber">Kortnummer*</label>
+            <input required type="tel" id="cardnumber" minLength="16" maxLength="16" inputMode="numeric" pattern="[0-9\s]{13,19}" />
+            <div className="expiry-cc-content">
+              <div className="expiry-content">
+                <label htmlFor="expiry">Gyldig til*</label>
+                <input required type="text" id="expiry" pattern="([0-9]{2}+[/]+?){2}" placeholder="MM/ÅÅ" minLength="4" maxLength="4" inputMode="numeric" />
+              </div>
+              <div className="cc-content">
+                <label htmlFor="cc">Kontrolcifre*</label>
+                <input required type="text" id="cc" minLength="3" maxLength="3" inputMode="numeric" />
+              </div>
+            </div>
           </div>
-          <div className="form-control">
+          {/* <div className="form-control">
             <input type="radio" name="paymentmethod" id="bankoverførsel" />
             <label htmlFor="bankoverførsel">Bankoverførsel</label>
-          </div>
+          </div> */}
 
           <Link to="/kvittering">
-            <button>Videre</button>
+            <input type="submit" />
           </Link>
         </form>
       </section>
