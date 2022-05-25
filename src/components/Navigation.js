21b9d19e763e66449c829dc9a492e72869994a7d
linegommesen <line090g@stud.kea.dk>
2022-05-25 10:39:07 +0200

    frontpage new background image

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -26,40 +26,13 @@ export default function Navigation() {
         </ul>
         <div className="logo-container">
           <Link to="/">
-            <img
-              src={`/images/logo_reviv.svg`}
-              alt="logo"
-              className="mobile-logo"
-            />
+            <img src={`/images/logo_reviv.svg`} alt="logo" className="mobile-logo" />
           </Link>
         </div>
         <Burgermenu />
       </nav>
 
       <Outlet></Outlet>
-
-      <style jsx>
-        {`
-          .navigation ul {
-            height: 100vh;
-            width: 50vw;
-            margin-top: 50px;
-            position: absolute;
-            list-style-type: none;
-          }
-
-          @media (min-width: 800px) {
-            .navigation ul {
-              display: flex;
-              justify-content: center;
-              gap: 95px;
-              width: 95%;
-              height: 50px;
-              //position: unset;
-            }
-          }
-        `}
-      </style>
     </>
   );
 }
