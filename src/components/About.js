ed776e225ca0f9ce7bac041cab4c1d9b0f506cc3
linegommesen <line090g@stud.kea.dk>
2022-05-25 12:47:10 +0200

    upcycling and about pages

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 export default function About() {
   return (
-    <>
+    <section className="about-section">
       <h1>Om Os</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit enim animi
-        sed eius nam at tempore libero ad dolores? Blanditiis distinctio aliquid
-        culpa necessitatibus ipsa quae placeat possimus recusandae corporis?
-      </p>
-    </>
+      <div className="about-content">
+        <p>
+          Réviv Studio opstod i 2020 ud fra en idé om at indsamle gamle smykker og give dem nyt liv gennem redesign. Réviv Studios smykker er håndlavet i Danmark, og produceres af en blanding af nye og genbrugte materialer. Smykkerne er derfor helt
+          unikke, og der findes kun én af hver slags. <br /> <br /> Bag virksomheden står jeg, Sarah, der har en bachelor i Smykker, Teknologi og Business fra KEA. Jeg har suppleret min uddannelse med diverse kurser, bl.a et år på kunsthøjskolen
+          Engelsholm. Jeg brænder for at skabe smykker, der fortæller en unik historie, og som skal kunne bæres i mange år. Min største drøm er at lykkes med dette, og samtidigt skabe den mest bæredygtige produktion. <br /> <br /> Du kan høre mere om
+          Réviv Studio i videoen.
+        </p>
+        <iframe src="https://www.youtube.com/embed/uu9MeCn2ECw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </section>
   );
 }
--- src/components/Upcycling.js
+++ src/components/Upcycling.js
@@ -1,14 +1,20 @@
 export default function Upcycling() {
   return (
     <div>
-      <section>
+      <section className="upcycling-first-section">
         <h1>Aflever dine smykker</h1>
-        <img src={`./images/kunderejse.png`} alt="" className="background-image" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eos iste, distinctio vero dolores nesciunt fugit perspiciatis neque atque minus sint accusantium sequi quaerat repellendus cum ducimus quae hic dicta?</p>
+        <div className="upcycling-content">
+          <p>
+            Réviv Studio skaber nye tidløse designs med materialer fra smykker, der ikke længere bruges. Dette kan f.eks. være fordi de er gået af mode, fordi ejeren ikke kan lide dem længere, eller fordi smykkerne er i stykker. På den måde kommer
+            materialerne i cirkulation igen, og går ikke til spilde. <br /> <br /> Noget af det bedste vi kan gøre i klimaets navn indenfor smykkebranchen, er at genanvende materialer, så der ikke skal trækkes nye op af jorden. <br /> <br /> Derfor
+            modtager vi hjertens gerne dine gamle smykker, og bruger materialerne til at lave nye, spændende designs. Som tak for din hjælp vil du altid modtage en rabatkode til vores webshops, der som minimum giver 10% rabat.
+          </p>
+          <img src={`./images/kunderejse.png`} alt="" className="background-image" />
+        </div>
       </section>
       <section>
         <h2>Her kan du aflevere dine smykker</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora accusamus enim quam obcaecati unde dolor ea. Non quisquam, est asperiores fugiat amet, architecto aperiam quaerat nulla eaque expedita impedit?</p>
+        <p></p>
       </section>
     </div>
   );
