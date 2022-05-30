2d94a747e914b676ed47b0ed9988cd02c5168aa0
linegommesen <line090g@stud.kea.dk>
2022-05-30 11:29:45 +0200

    image size fixed

--- src/components/Upcycling.js
+++ src/components/Upcycling.js
@@ -16,23 +16,11 @@ export default function Upcycling(props) {
         <h1>Aflever dine smykker</h1>
         <div className="upcycling-content">
           <p>
-            Réviv Studio skaber nye tidløse designs med materialer fra smykker,
-            der ikke længere bruges. Dette kan f.eks. være fordi de er gået af
-            mode, fordi ejeren ikke kan lide dem længere, eller fordi smykkerne
-            er i stykker. På den måde kommer materialerne i cirkulation igen, og
-            går ikke til spilde. <br /> <br /> Noget af det bedste vi kan gøre i
-            klimaets navn indenfor smykkebranchen, er at genanvende materialer,
-            så der ikke skal trækkes nye op af jorden. <br /> <br /> Derfor
-            modtager vi hjertens gerne dine gamle smykker, og bruger
-            materialerne til at lave nye, spændende designs. Som tak for din
-            hjælp vil du altid modtage en rabatkode til vores webshops, der som
-            minimum giver 10% rabat.
+            Réviv Studio skaber nye tidløse designs med materialer fra smykker, der ikke længere bruges. Dette kan f.eks. være fordi de er gået af mode, fordi ejeren ikke kan lide dem længere, eller fordi smykkerne er i stykker. På den måde kommer
+            materialerne i cirkulation igen, og går ikke til spilde. <br /> <br /> Noget af det bedste vi kan gøre i klimaets navn indenfor smykkebranchen, er at genanvende materialer, så der ikke skal trækkes nye op af jorden. <br /> <br /> Derfor
+            modtager vi hjertens gerne dine gamle smykker, og bruger materialerne til at lave nye, spændende designs. Som tak for din hjælp vil du altid modtage en rabatkode til vores webshops, der som minimum giver 10% rabat.
           </p>
-          <img
-            src={`./images/kunderejse.png`}
-            alt=""
-            className="background-image"
-          />
+          <img src={`./images/kunderejsen_web.png`} alt="" className="background-image" />
         </div>
       </section>
       <section>
